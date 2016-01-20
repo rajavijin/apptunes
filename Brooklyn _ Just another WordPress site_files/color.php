949e720f83ed9a3f6ee271345d21e1825bed7f3f
sahaya rex <rexdeveloper@gmail.com>
2016-01-21 03:32:46 +0530

    initial commit

---
+++ Brooklyn _ Just another WordPress site_files/color.php
@@ -0,0 +1,8 @@
+
+#our-service .fa { color: #4CDDF3 !important; }
+#core-features .fa-circle { color: #4CDDF3 !important; }
+.ut-left-footer-area .fa-map-marker,
+.ut-left-footer-area .fa-phone,
+.ut-left-footer-area .fa-envelope-o {
+	color: #4CDDF3 !important;
+}
